75f6537c51e0ca575a83548e1bca26b2d65a3f01
Jawrynx <jawrynx@gmail.com>
2024-12-25 10:59:47 +0000

    Updated Project Name

--- ErdPL/settings.py
+++ ErdPL/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'ErdPSL.urls'
+ROOT_URLCONF = 'ErdPL.urls'
 
 TEMPLATES = [
     {
--- ErdPL/wsgi.py
+++ ErdPL/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ErdPSL.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ErdPL.settings')
 
 application = get_wsgi_application()
