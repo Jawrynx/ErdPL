73e8d74b8a5abfd95696ac09d49e0b6aeb47a50c
Jawrynx <jawrynx@gmail.com>
2024-12-31 14:57:15 +0000

    More styling for Layout.html in Navbar

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,9 @@
 const menuToggle = document.getElementById('menu-toggle');
 const mobileNav = document.getElementById('mobile-nav');
+const navBar = document.getElementById('header');
+
 
 menuToggle.addEventListener('click', () => {
     mobileNav.classList.toggle('active');
+    navBar.classList.toggle('mobile-active');
 });
