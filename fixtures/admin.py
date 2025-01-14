7c6a6128892f9f56ffc707567822d05072fbd1b2
Jawrynx <jawrynx@gmail.com>
2025-01-14 10:34:34 +0000

    Styling for Teams Profile and Fixtures. Added Team Profile links into Navbar if User has a team.

--- fixtures/admin.py
+++ fixtures/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Fixture
 
 class FixtureAdmin(admin.ModelAdmin):
-    list_display = ('date', 'home_team', 'away_team', 'division', 'home_or_away') 
+    list_display = ('date', 'home_team', 'away_team', 'division') 
     list_filter = ('division', 'date') 
     date_hierarchy = 'date' 
 
--- fixtures/models.py
+++ fixtures/models.py
@@ -6,8 +6,6 @@ class Fixture(models.Model):
     away_team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='away_fixtures')
     date = models.DateField()
     division = models.ForeignKey(Division, on_delete=models.CASCADE)
-    HOME_AWAY_CHOICES = [('HOME', 'Home'), ('AWAY', 'Away'),]
-    home_or_away = models.CharField(max_length=5, choices=HOME_AWAY_CHOICES, default='HOME')
 
     def __str__(self):
         return f"{self.home_team} vs {self.away_team} on {self.date}"
