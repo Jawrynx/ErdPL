146288b6d64d31ea0dfd3df1c44ead28a695b93b
Jawrynx <jawrynx@gmail.com>
2025-01-17 12:52:18 +0000

    Added more detail to home page

--- ErdPL/views.py
+++ ErdPL/views.py
@@ -4,28 +4,7 @@ from django.shortcuts import render
 import random
 
 def home(request):
-    teams = ['Team A', 'Team B', 'Team C', 'Team D', 'Team E', 'Team F', 'Team G', 'Team H', 'Team I', 'Team J']
-    results_table = []
-
-    for _ in range(20):
-        home_team = random.choice(teams)
-        away_team = random.choice(teams)
-        while away_team == home_team:
-            away_team = random.choice(teams)
-        home_score = random.randint(0, 10)
-        away_score = random.randint(0, 10)
-        date = f'2024-12-{random.randint(1, 31)}'  # Generate random dates in December 2024
-
-        results_table.append({
-            'home_team': home_team,
-            'away_team': away_team,
-            'home_score': home_score,
-            'away_score': away_score,
-            'date': date
-        })
-
-    context = {'results_table': results_table}
-    return render(request, 'home.html', context)
+    return render(request, 'home.html')
 
 def about(request):
     # return HttpResponse('Hello, I am the About Page!')
