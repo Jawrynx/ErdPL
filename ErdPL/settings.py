47953188cc804053c39dbe1a11994e2ac478b1e0
Jawrynx <jawrynx@gmail.com>
2025-02-03 13:29:50 +0000

    Added team photo

--- ErdPL/settings.py
+++ ErdPL/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+# Build paths inside the project like this: BASE_DIR / 'subdir'.!
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4=om+%pih5u954)#h5bni$vb92(^mx3kuan%86^dtx#yz%3b)5'
 
 # SECURITY WARNING: don't run with debug turned on in production!!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
