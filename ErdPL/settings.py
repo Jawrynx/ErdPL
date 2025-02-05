f3c8655591061eab21819d6cc5613547b95b7219
Jawrynx <jawrynx@gmail.com>
2025-02-05 10:44:58 +0000

    Added Postgres, migrated data

--- ErdPL/settings.py
+++ ErdPL/settings.py
@@ -87,12 +87,17 @@ from dotenv import load_dotenv
 
 load_dotenv()  # Load environment variables from .env file
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
     }
-}
+else:  # Development environment (SQLite)
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),  # Path to your SQLite database
+        }
+    }
 
 AUTH_USER_MODEL = 'users.CustomUser' 
 
