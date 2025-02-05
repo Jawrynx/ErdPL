46aacc10e800e383d7c262de89a453c46995e89b
Jawrynx <jawrynx@gmail.com>
2025-02-05 15:17:25 +0000

    Fixed Leave button incorrectly displaying, added links to matches in team profiles

--- ErdPL/settings.py
+++ ErdPL/settings.py
@@ -91,11 +91,11 @@ if 'DATABASE_URL' in os.environ:
     DATABASES = {
         'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
     }
-else:  # Development environment (SQLite)
+else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),  # Path to your SQLite database
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
 
