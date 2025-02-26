a7ea70280782f86b4fa3446f9f71017d08dd43c2
Jawrynx <jawrynx@gmail.com>
2025-02-26 15:50:28 +0000

    Created Player Models and ability to add players without user account

--- ErdPL/settings.py
+++ ErdPL/settings.py
@@ -90,17 +90,17 @@ from dotenv import load_dotenv
 
 load_dotenv()  # Load environment variables from .env file
 
-"""if 'DATABASE_URL' in os.environ:
+if 'DATABASE_URL' in os.environ:
     DATABASES = {
         'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
     }
-else:"""
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
 
 AUTH_USER_MODEL = 'users.CustomUser' 
 
