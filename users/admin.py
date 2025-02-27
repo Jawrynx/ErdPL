62267c88758877a8b6c4eb16fd06babd97b82c80
Jawrynx <jawrynx@gmail.com>
2025-02-27 14:59:44 +0000

    Added Player models to Database and adjusted a few data entries

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from.models import CustomUser
+from.models import CustomUser, Player
 
 class CustomUserAdmin(BaseUserAdmin):
     list_display = ('username', 'fullname', 'profile_picture', 'phone_number', 'bio', 'get_team') 
@@ -12,4 +12,8 @@ class CustomUserAdmin(BaseUserAdmin):
 
     get_team.short_description = 'Team' 
 
+class PlayerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'match_wins', 'game_wins', 'total_games')
+
 admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(Player, PlayerAdmin)
