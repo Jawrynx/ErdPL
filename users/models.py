93c7ea6297c1e80a68af9c95fa67223b67775fa0
Jawrynx <jawrynx@gmail.com>
2025-03-02 19:25:37 +0000

    Removed team field from CustomUser model as it was conflicting with another models attribute

--- users/models.py
+++ users/models.py
@@ -6,7 +6,6 @@ class CustomUser(AbstractUser):
     profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
     phone_number = models.CharField(max_length=20, blank=True, null=True)
     bio = models.TextField(blank=True)
-    team = models.OneToOneField('teams.Team', on_delete=models.SET_NULL, null=True, blank=True, related_name='team_members')
 
     def __str__(self):
         return self.username 
